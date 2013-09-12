ce7920e1dfb344617d42d2f4c77cd516966348ba
Carl Schwope <schwope.carl@gmail.com>
2013-09-12 16:07:19 -0400

    gives notice on successful creation, pass test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,22 @@ class CommentsController < ApplicationController
   end
 
   def new
+    # Sam, I was getting 'no method' for Comment.build
     @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new( comment_params )
+
+    if @comment.save
+      redirect_to '/comments', notice: "Thanks for the comment"
+    else
+      redirect_to '/comments', notice: 'nope'
+    end
+  end
+
+  def comment_params
+    params.require( :comment ).permit( :email, :subject, :description, :first_name, :last_name )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DogHub::Application.routes.draw do
 
-  resources 'comments', only: [:index, :new]
+  resources 'comments', only: [:index, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
